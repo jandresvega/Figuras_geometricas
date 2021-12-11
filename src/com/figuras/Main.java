28f2c125f3366c9906489171875722c87997f496
andrebass <andre300.av@gmail.com>
2021-12-11 12:51:23 -0500

    subir proyecto java con el menu de opciones

---
+++ src/com/figuras/Main.java
@@ -0,0 +1,39 @@
+package com.figuras;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Scanner entrada=new Scanner(System.in);
+        boolean contador=true;
+
+        do {
+            System.out.println("***** MENU *****");
+            System.out.println("1) Cuadrado");
+            System.out.println("2) Triangulo Rectangulo");
+            System.out.println("3) Triangulo Isosceles");
+            System.out.println("4) Rombo");
+            System.out.println("5) Salir");
+
+            int opc= entrada.nextInt();
+
+            switch (opc){
+                case 1:
+                    break;
+                case 2:
+                    System.out.println("triangulo Rectangulo");
+                    break;
+                case 3:
+                    System.out.println("Triangulo Isosceles");
+                    break;
+                case 4:
+                    System.out.println("Rombo");
+                default:
+                    contador=false;
+                    break;
+            }
+        }while(contador);
+    }
+}
